1910a4aa968c4a35e8c333581016958789645ee2
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2022-11-10 19:17:39 +0530

    day2: tested and working for a few inputs

--- 2015/src/Day2.hs
+++ 2015/src/Day2.hs
@@ -1,2 +1,42 @@
 module Day2 where
 
+type Length = Int
+type Width = Int
+type Height = Int
+
+data Box = Box {
+  l :: Length
+  ,w :: Width
+  ,h :: Height
+  } deriving (Show, Eq)
+
+split :: String -> Char -> [String]
+split s c = go s c []
+  where go s c acc | s == "" = acc
+                   | otherwise =
+                     let x = takeWhile (/= c) s
+                         rs = drop (length x + 1) s
+                     in
+                       go rs c (acc ++ [x])
+
+
+strToDimension :: String -> Box
+strToDimension s =
+  let numList = map read $ split s 'x'
+  in
+    Box (numList !! 0) (numList !! 1) (numList !! 2)
+
+
+area :: Int -> Int -> Int
+area x y = x * y
+
+paperArea :: Box -> Int
+paperArea b =
+  let area1 = area (l b) (w b)
+      area2 = area (w b) (h b)
+      area3 = area (h b) (l b)
+      minArea = minimum [area1, area2, area3]
+  in
+    2*area1 + 2*area2 + 2*area3 + minArea
+
+
